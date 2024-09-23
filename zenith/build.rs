c0e8ffde0c9fce2172c1c92561652a68e3b6a209
Reun <gugigergo@gmail.com>
2024-09-24 00:02:49 +0200

    Added configurations for project

---
+++ zenith/build.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    // Detect which profile is being used and define the corresponding flag
+    let profile = std::env::var("PROFILE").unwrap();
+
+    if profile == "debug" {
+        println!("cargo:rustc-cfg=build_type=\"debug\"");
+    } else if profile == "release" {
+        println!("cargo:rustc-cfg=build_type=\"release\"");
+    } else if profile == "dist" {
+        println!("cargo:rustc-cfg=build_type=\"dist\"");
+    }
+}
