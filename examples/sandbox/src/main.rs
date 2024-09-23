83b588123ac9ee91fb01c9ae4cbc61a8907d9998
Reun <gugigergo@gmail.com>
2024-09-23 23:26:02 +0200

    Add error handling to main

--- examples/sandbox/src/main.rs
+++ examples/sandbox/src/main.rs
@@ -7,6 +7,29 @@
     clippy::style
 )]
 
-fn main() {
-    println!("Hello, world!");
+use zenith::log;
+use zenith::Application;
+
+#[derive(Debug)]
+struct App {}
+
+impl Application for App {
+    type BuildError = ();
+
+    type RunError = ();
+
+    fn build() -> Result<Self, Self::BuildError> {
+        log::info!("Building application");
+        Ok(Self {})
+    }
+
+    fn run(&mut self) -> Result<(), Self::RunError> {
+        log::info!("Ran application");
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    zenith::start_application::<App>()?;
+    Ok(())
 }
