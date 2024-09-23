cbc45fb004f2af7053a1acb26ac7722d4ff73f35
Reun <gugigergo@gmail.com>
2024-09-23 23:27:45 +0200

    Entry point

--- zenith/src/lib.rs
+++ zenith/src/lib.rs
@@ -7,10 +7,52 @@
     clippy::style
 )]
 
+pub mod event;
 pub mod log;
 pub mod shaders;
 pub mod undrop;
+pub mod window;
 
-fn main() -> Result<(), ()> {
-    Ok(())
+pub trait Application
+where
+    Self: Sized,
+{
+    type BuildError: std::fmt::Debug;
+    type RunError: std::fmt::Debug;
+
+    fn build() -> Result<Self, Self::BuildError>;
+    fn run(&mut self) -> Result<(), Self::RunError>;
 }
+
+#[derive(Debug, thiserror::Error)]
+pub enum UserError<App: Application> {
+    #[error("User Application run returned with error {0:?}")]
+    RunError(App::RunError),
+    #[error("User Application build returned with error {0:?}")]
+    BuildError(App::BuildError),
+}
+#[derive(Debug, thiserror::Error)]
+pub enum Error<App: Application> {
+    #[error(transparent)]
+    External(#[from] UserError<App>),
+    #[error(transparent)]
+    LoggerError(#[from] log::Error),
+}
+
+pub fn start_application<App: Application>() -> Result<(), Error<App>> {
+    log::create(log::LoggingCreateInfo {
+        level: log::Level::TRACE,
+        ..log::LoggingCreateInfo::max()
+    })?;
+    let mut app = match App::build() {
+        Ok(x) => x,
+        Err(e) => return Err(Error::External(UserError::BuildError(e))),
+    };
+
+    match app.run() {
+        Ok(_) => Ok(()),
+        Err(e) => Err(Error::External(UserError::RunError(e))),
+    }
+}
+
+type PixelCoordinate = nalgebra::Vector2<u32>;
