1b79ac5178769d1064cf3c0da5f0953f478fd161
Reun85 <gugigergo@gmail.com>
2024-09-25 18:37:43 +0200

    Updated Event system to be more generic

--- zenith/src/event/input.rs
+++ zenith/src/event/input.rs
@@ -1,69 +1,143 @@
+#![allow(unused_imports)]
+
 #[derive(Debug, derive_more::From, derive_more::IsVariant, derive_more::TryInto)]
 pub enum Event {
-    KeyBoard(keyboard::KeyBoardEvent),
-    Mouse(mouse::MouseEvent),
-    Window(window::WindowEvent),
+    KeyBoard(keyboard::Event),
+    Mouse(mouse::Event),
+    Window(window::Event),
 }
 
+bitflags::bitflags! {
+    #[repr(transparent)]
+    #[derive(Debug, Clone, Copy, PartialEq,Eq, PartialOrd, Ord,Hash)]
+    pub struct EventCategories: u32 {
+        const None= 1u32<<0;
+        const Input = 1u32 << 1;
+        const Keyboard = 1u32<<2;
+        const Mouse = 1u32<<3;
+        const Window = 1u32<<4;
+
+
+        const InputAndKeyboard = Self::Input.bits() | Self::Keyboard.bits();
+        const InputAndMouse = Self::Input.bits() | Self::Mouse.bits();
+    }
+}
+
+use super::EventLike;
+use super::HasStaticCategory;
+
 pub mod keyboard {
+    use super::{EventCategories, EventLike, HasStaticCategory};
 
     #[derive(Debug, derive_more::From, derive_more::IsVariant, derive_more::TryInto)]
-    pub enum KeyBoardEvent {
-        KeyPress(KeyPressEvent),
-        KeyRelease(KeyReleaseEvent),
+    pub enum Event {
+        KeyPress(PressEvent),
+        KeyRelease(ReleaseEvent),
     }
     #[derive(Debug, derive_more::From)]
     pub struct Key {}
     #[derive(Debug, derive_more::From)]
-    pub struct KeyPressEvent {
+    pub struct PressEvent {
         pub key: Key,
         pub repeat: bool,
     }
 
     #[derive(Debug, derive_more::From)]
-    pub struct KeyReleaseEvent {
+    pub struct ReleaseEvent {
         pub key: Key,
+    }
+
+    impl HasStaticCategory for Event {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndKeyboard;
+    }
+    impl HasStaticCategory for PressEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndKeyboard;
+    }
+    impl HasStaticCategory for ReleaseEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndKeyboard;
     }
 }
 pub mod mouse {
+    use super::{EventCategories, EventLike, HasStaticCategory};
     #[derive(Debug, derive_more::From)]
     pub struct Button {}
     #[derive(Debug, derive_more::From, derive_more::IsVariant, derive_more::TryInto)]
-    pub enum MouseEvent {
-        MousePress(MousePressEvent),
-        MouseRelease(MouseReleaseEvent),
-        MouseMove(MouseMoveEvent),
+    pub enum Event {
+        MousePress(PressEvent),
+        MouseRelease(ReleaseEvent),
+        MouseMove(MoveEvent),
+    }
+    impl HasStaticCategory for Event {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndMouse;
     }
     #[derive(Debug, derive_more::From)]
 
-    pub struct MouseReleaseEvent {
+    pub struct ReleaseEvent {
         pub button: Button,
     }
+    impl HasStaticCategory for ReleaseEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndMouse;
+    }
 
     #[derive(Debug, derive_more::From)]
-    pub struct MousePressEvent {
+    pub struct PressEvent {
         pub button: Button,
         pub repeat: bool,
     }
+    impl HasStaticCategory for PressEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndMouse;
+    }
 
     #[derive(Debug, derive_more::From)]
-    pub struct MouseMoveEvent {
+    pub struct MoveEvent {
         pub position: crate::PixelCoordinate,
+    }
+    impl HasStaticCategory for MoveEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::InputAndMouse;
     }
 }
 
 pub mod window {
+    use super::{EventCategories, HasStaticCategory};
     #[derive(Debug, derive_more::From, derive_more::IsVariant, derive_more::TryInto)]
-    pub enum WindowEvent {
-        WindowClose(WindowCloseEvent),
-        WindowResize(WindowResizeEvent),
+    pub enum Event {
+        WindowClose(CloseEvent),
+        WindowResize(ResizeEvent),
+    }
+    impl HasStaticCategory for Event {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::Window;
     }
     #[derive(Debug, derive_more::From)]
-    pub struct WindowCloseEvent {}
+    pub struct CloseEvent {}
+    impl HasStaticCategory for CloseEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::Window;
+    }
     #[derive(Debug, derive_more::From)]
-    pub struct WindowResizeEvent {
+    pub struct ResizeEvent {
         pub size: crate::PixelCoordinate,
+    }
+    impl HasStaticCategory for ResizeEvent {
+        type Category = EventCategories;
+        const CATEGORY: EventCategories = EventCategories::Window;
     }
 }
 
-impl super::EventLike for Event {}
+impl super::EventLike for Event {
+    type Category = EventCategories;
+    fn get_category(&self) -> EventCategories {
+        match self {
+            Event::KeyBoard(x) => x.get_category(),
+            Event::Mouse(x) => x.get_category(),
+            Event::Window(x) => x.get_category(),
+        }
+    }
+}
--- zenith/src/event/mod.rs
+++ zenith/src/event/mod.rs
@@ -1,17 +1,107 @@
 pub mod input;
 
-/// Marker Trait for data types that may be dispatch using  `EventDispatcher`
+/// Marker Trait
+pub trait EventCategory
+where
+    Self: crate::infrastructure::Debug,
+{
+    fn contains(&self, other: &Self) -> bool;
+}
+
+// TODO: Change this to own Flags impl trait
+// impl EventCategory for () {
+//     fn contains(&self, other: &Self) -> bool {
+//         true
+//     }
+// }
+impl<T> EventCategory for T
+where
+    T: bitflags::Flags + crate::infrastructure::Debug + Clone,
+{
+    fn contains(&self, other: &Self) -> bool {
+        bitflags::Flags::contains(self, other.clone())
+    }
+}
+
+/// Marker Trait for data types that may be dispatched using  `EventDispatcher`
 pub trait EventLike
 where
-    Self: Sized + std::fmt::Debug,
+    Self: crate::infrastructure::Debug,
 {
+    /// The `EventCategory` this Event belongs to
+    /// set to () if you don't wish to use `EventCategory`-s
+    type Category: EventCategory;
+    fn get_category(&self) -> Self::Category;
+}
+
+pub trait HasStaticCategory
+where
+    Self: std::fmt::Debug,
+{
+    type Category: EventCategory;
+    const CATEGORY: Self::Category;
+}
+
+impl<T: HasStaticCategory> EventLike for T {
+    type Category = T::Category;
+
+    fn get_category(&self) -> Self::Category {
+        T::CATEGORY
+    }
 }
 
 /// Marker Trait for data types that may can be a callback for `EventDispatcher`
 pub trait EventCallbackLike<E: EventLike> {
-    fn call(&self, event: &E);
+    /// Returns whether the event was handled
+    fn call(&self, event: &E) -> bool;
 }
 
-pub trait EventDispatcher {
-    fn dispatch<E: EventLike>(&self, callback: impl EventCallbackLike<E>);
+impl<E, F> EventCallbackLike<E> for F
+where
+    E: EventLike,
+    F: Fn(&E) -> bool,
+{
+    fn call(&self, event: &E) -> bool {
+        self(event)
+    }
+}
+
+pub trait Dispatcher<E: EventLike> {
+    fn dispatch<F: HasStaticCategory<Category = E::Category>>(
+        &mut self,
+        callback: impl EventCallbackLike<E>,
+    );
+}
+
+#[derive(Debug, derive_more::From)]
+pub struct BasicDispatcher<E>
+where
+    E: EventLike,
+{
+    event: E,
+    handled: bool,
+}
+
+impl<E: EventLike> BasicDispatcher<E> {
+    fn new(event: E) -> Self {
+        Self {
+            event,
+            handled: false,
+        }
+    }
+}
+
+impl<E: EventLike> Dispatcher<E> for BasicDispatcher<E> {
+    fn dispatch<F: HasStaticCategory<Category = E::Category>>(
+        &mut self,
+        callback: impl EventCallbackLike<E>,
+    ) {
+        if !self.handled {
+            return;
+        }
+        let category = self.event.get_category();
+        if category.contains(&F::CATEGORY) {
+            self.handled = callback.call(&self.event);
+        }
+    }
 }
